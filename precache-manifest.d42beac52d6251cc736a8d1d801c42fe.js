884b6746b8f42a27931424a94b0f252fc7957376
NinjaDevelopers <ninjadevelopers4@gmail.com>
2020-10-24 10:52:02 +0530

    Updates

---
+++ precache-manifest.d42beac52d6251cc736a8d1d801c42fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa2fee422d2f0e42c7214eb773eb3d4",
+    "url": "/ninja-developers.github.io/index.html"
+  },
+  {
+    "revision": "ad3839e1aa4fa529517a",
+    "url": "/ninja-developers.github.io/static/js/2.2bfd3efa.chunk.js"
+  },
+  {
+    "revision": "7d8dbc766008266d801fb16548a87b19",
+    "url": "/ninja-developers.github.io/static/js/2.2bfd3efa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b3e368c074bfa54ae9f",
+    "url": "/ninja-developers.github.io/static/js/main.4f6e1238.chunk.js"
+  },
+  {
+    "revision": "7f2ebb9a72abd6739769",
+    "url": "/ninja-developers.github.io/static/js/runtime-main.d7e59da7.js"
+  }
+]);
